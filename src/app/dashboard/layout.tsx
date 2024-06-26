9edb1b24ea0427421b3889ab5b255ddb31214eb3
CESCO <cescovalledev@gmail.com>
2024-06-26 15:38:02 -0600

    next image added

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function DashboardLayout({
   children,
 }: {
@@ -22,10 +24,14 @@ export default function DashboardLayout({
             <p className="text-slate-500">Welcome back,</p>
             <a href="#" className="inline-flex space-x-2 items-center">
               <span>
-                <img
-                  className="rounded-full w-8 h-8"
-                  src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80"
-                  alt=""
+                <Image
+                  className="rounded-full"
+                  src={
+                    "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80"
+                  }
+                  alt={"profile-pic"}
+                  width={32}
+                  height={32}
                 />
               </span>
               <span className="text-sm md:text-base font-bold">
