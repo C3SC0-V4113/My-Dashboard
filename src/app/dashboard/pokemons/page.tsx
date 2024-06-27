7ce20ee8915d269557557b1d68e46869578a3f12
CESCO <cescovalledev@gmail.com>
2024-06-26 23:51:39 -0600

    added interfaces to fetch

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,12 +1,35 @@
-const getPokemons = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((response) => response.json());
 
-  return data;
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
 };
 
 export default async function PokemonsPage() {
-  const pokemons = await getPokemons();
-  return <div>{JSON.stringify(pokemons)}</div>;
+  const pokemons = await getPokemons(151);
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${10}.png`}
+          alt={`Pokemon ${10}`}
+          width={100}
+          height={100}
+        />
+      </div>
+      {JSON.stringify(pokemons)}
+    </div>
+  );
 }
