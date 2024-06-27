4383f01ec82a155d2ad847d1a09e0c807fd44f9d
CESCO <cescovalledev@gmail.com>
2024-06-26 18:03:46 -0600

    state added to counter

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { Metadata } from "next";
+import { useState } from "react";
+
+// export const metadata: Metadata = {
+//   title: "Counter Page",
+//   description: "Un Simple Contador",
+// };
+
 export default function CounterPage() {
+  const [counter, setCounter] = useState(10);
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{counter}</span>
+
+      <div className="flex">
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => setCounter((prevState) => prevState + 1)}
+        >
+          +1
+        </button>
+        <button
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => setCounter((prevState) => prevState - 1)}
+        >
+          -1
+        </button>
+      </div>
     </div>
   );
 }
