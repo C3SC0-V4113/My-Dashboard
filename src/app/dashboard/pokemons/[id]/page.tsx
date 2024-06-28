662aec00ca1ff8a0fab6deac5f7e0a4eae551689
CESCO <cescovalledev@gmail.com>
2024-06-27 22:26:23 -0600

    Pokemon types added

--- src/app/dashboard/pokemons/[id]/page.tsx
+++ src/app/dashboard/pokemons/[id]/page.tsx
@@ -1,13 +1,29 @@
+import { Pokemon } from "@/pokemons";
 import { NextPage } from "next";
+
+export const metadata = {
+  title: "SEO Title",
+  description: "SEO Title",
+};
 
 interface Props {
   params: { id: string };
 }
 
-const PokemonPage: NextPage<Props> = ({ params }) => {
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  }).then((resp) => resp.json());
+
+  return pokemon;
+};
+
+const PokemonPage: NextPage<Props> = async ({ params }) => {
+  const pokemon = await getPokemon(params.id);
   return (
     <div>
       <h1>Pokémon {params.id}</h1>
+      <div className="">{JSON.stringify(pokemon)}</div>
     </div>
   );
 };
