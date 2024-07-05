5a48d0f7b6b2caa49266d5f99b4862bab2e0e567
CESCO <cescovalledev@gmail.com>
2024-07-05 16:34:21 -0600

    counter with redux

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,27 +1,31 @@
 "use client";
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { addOne, substractOne } from "@/store/counter/counterSlice";
+// import { useState } from "react";
 
 interface Props {
   value?: number;
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value);
+  // const [counter, setCounter] = useState(value);
 
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <span className="text-9xl">{counter}</span>
+      <span className="text-9xl">{count}</span>
 
       <div className="flex">
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCounter((prevState) => prevState + 1)}
+          onClick={() => dispatch(addOne())}
         >
           +1
         </button>
         <button
           className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCounter((prevState) => prevState - 1)}
+          onClick={() => dispatch(substractOne())}
         >
           -1
         </button>
