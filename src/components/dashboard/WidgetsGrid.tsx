72f2a343b70c754e702a0bd48ee575a754a15140
CESCO <cescovalledev@gmail.com>
2024-07-07 23:08:12 -0600

    work done?

--- src/components/dashboard/WidgetsGrid.tsx
+++ src/components/dashboard/WidgetsGrid.tsx
@@ -1,12 +1,15 @@
+"use client";
 import { IoCafeOutline } from "react-icons/io5";
 import { SimpleWidget } from "./SimpleWidget";
+import { useAppSelector } from "@/hooks";
 
 export const WidgetsGrid = () => {
+  const { count } = useAppSelector((state) => state.counter);
   return (
     <div className="flex flex-wrap p-2 items-center justify-center">
       <SimpleWidget
         label="Counter"
-        title={20}
+        title={count}
         subTitle="Productos en el carrito"
         href="/dashboard/counter"
         icon={<IoCafeOutline size={50} className="text-blue-500" />}
